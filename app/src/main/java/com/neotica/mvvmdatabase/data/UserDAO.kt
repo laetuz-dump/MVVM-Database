0c4aa11b0b2cabfa6741e8024baa1472d7116329
Ryo Martin <laetuzg@gmail.com>
2022-11-09 00:39:07 +0700

    Add @Dao annotation for UserDAO.kt interface.

--- app/src/main/java/com/neotica/mvvmdatabase/data/UserDAO.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/data/UserDAO.kt
@@ -1,14 +1,16 @@
 package com.neotica.mvvmdatabase.data
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+@Dao
 interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(user: User)
 
-    @Query(value = "SELECT * FROM user_table ORDER BY id ASC")
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
 }
