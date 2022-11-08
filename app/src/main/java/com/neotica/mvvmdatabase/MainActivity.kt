fbd9ece082cff1dc7e3b84dfabce83aa556ab693
Ryo Martin <laetuzg@gmail.com>
2022-11-09 04:43:32 +0700

    Created ActionBar with navigateUp support.

--- app/src/main/java/com/neotica/mvvmdatabase/MainActivity.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.neotica.mvvmdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Step 16: Use method setupActionBarWithNavController
+        //Step 17: Pass findNavController as a parameter
+        //Step 18: Pass the id of NavHostFragment
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+    }
+
+    //Step 19: Override onSupportNavigateUp to use the back button on the action bar.
+    override fun onSupportNavigateUp(): Boolean {
+        //Step 20: Create NavController object and specify fragment
+        val navController = findNavController(R.id.fragmentContainerView)
+        //Step 21: Return NavController NavigateUp
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
