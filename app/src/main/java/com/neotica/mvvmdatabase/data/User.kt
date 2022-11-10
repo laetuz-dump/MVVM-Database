86301e06a7321f5b6277d33287420c627e79395a
Ryo Martin <laetuzg@gmail.com>
2022-11-11 01:29:21 +0700

    Parcelized the Entity class.

--- app/src/main/java/com/neotica/mvvmdatabase/data/User.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/data/User.kt
@@ -1,12 +1,17 @@
 package com.neotica.mvvmdatabase.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+//Step 66: Parcelize Entity by adding @Parcelize annotation
+@Parcelize
 @Entity(tableName = "user_table")
 data class User (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
     val age: Int
-    )
+    //Step 67: Extends Parcelable
+    ):Parcelable
