5a19205338402d9ae4a705810ad9efe6eb910ef1
Ryo Martin <laetuzg@gmail.com>
2022-11-10 12:20:11 +0700

    Added and initialized recyclerView from database into ListFragment.kt.

--- app/src/main/java/com/neotica/mvvmdatabase/fragments/list/ListFragment.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/fragments/list/ListFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neotica.mvvmdatabase.R
+import com.neotica.mvvmdatabase.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+    //Step 54
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,26 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         //Step 11: Set/store inflatedLayout in the view variable.
         val view1 = inflater.inflate(R.layout.fragment_list, container, false)
+
+        //RecyclerView
+        //Step 55: Add recyclerView to the inflated view
+        val recyclerView = view1.recView
+        //Step 56: Create adapter variable on listAdapter
+        val adapter = ListAdapter()
+        //Step 57: Set adapter for the recyclerView
+        recyclerView.adapter = adapter
+        //Step 58: Set layoutManager  to linear layoutManager, set requireContext
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        //UserViewModel
+        //Step 59: Initialize UserViewModel, use ViewModelProvider
+        //Step 60: Pass the context to this class and get the context from  UserViewModel.kt
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        //Step 61: readAllData from listview using Observer
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer {
+            user ->
+            adapter.setData(user)
+        })
 
         //Step 13: Use view1 to find the FAB and setOnClickListener.
         view1.floatingActionButton.setOnClickListener {
