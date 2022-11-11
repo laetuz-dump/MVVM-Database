3c6bf29cb92be01942d9a7ed3b8444e7e06eb7b3
Ryo Martin <laetuzg@gmail.com>
2022-11-12 01:00:51 +0700

    Created update function for the room database.

--- app/src/main/java/com/neotica/mvvmdatabase/data/UserRepository.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/data/UserRepository.kt
@@ -8,4 +8,13 @@ class UserRepository (private val userDAO: UserDAO){
     suspend fun addUser(user: User){
         userDAO.addUser(user)
     }
+
+    //Step 95: Add a new suspended function, pass the Entity as the parameter
+    suspend fun updateUser(user: User){
+        //Step 96: Call Dao, use the updateUser function, pass user
+        userDAO.updateUser(user)
+    }
+
+    //Step 97: Open ViewModel class
+
 }
