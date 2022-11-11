9e02769ece468b1beb04a8489a2c21b592ddace5
Ryo Martin <laetuzg@gmail.com>
2022-11-12 01:00:39 +0700

    Created update function and annotation for the room database.

--- app/src/main/java/com/neotica/mvvmdatabase/data/UserDAO.kt
+++ app/src/main/java/com/neotica/mvvmdatabase/data/UserDAO.kt
@@ -5,11 +5,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(user: User)
+
+    //Step 93: Add the @Update annotation
+    @Update
+    //Step 94: Create a new suspended function (Because Coroutines) and pass the Entity
+    suspend fun updateUser(user: User)
+    //Step 95: Open Repository Class
 
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
